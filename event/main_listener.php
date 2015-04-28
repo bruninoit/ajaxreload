cccc1ad60827817b44974649b384c69798c93b87
bruninoit <brunino98@hotmail.it>
2015-04-28 21:35:01 +0200

    listener

---
+++ event/main_listener.php
@@ -0,0 +1,43 @@
+<?php
+/**
+*
+* @package ajaxreload
+* @copyright (c) 2013 Bruninoit
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+namespace bruninoit\ajaxreload\event;
+/**
+* @ignore
+*/
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+/**
+* Event listener
+*/
+
+class main_listener implements EventSubscriberInterface
+{
+	//change true -> false to disable the options
+	$whoisonline = true; //refresh who is online
+	$forumlist = true;   //refresh forum list
+	
+	static public function getSubscribedEvents()
+	{
+		return array(
+			'core.common'						=> 'variables_refresh',
+			);
+	}
+	/* @var \phpbb\template\template */
+	protected $config;
+
+	public function __construct(\phpbb\config\config $config)
+	{
+        $this->$config = $config;
+	}
+	public function variables_refresh($event)
+	{
+	//change true to false to disable the options
+	$this->template->assign_var('FORUMLIST_RELOAD', $forumlist); 
+	$this->template->assign_var('WHOISONLINE_RELOAD', $whoisonline);
+	}
+}
