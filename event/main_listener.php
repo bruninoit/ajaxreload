7ab8e3f02526c426182c04243ed3513047c060e9
bruninoit <brunino98@hotmail.it>
2015-04-29 14:00:57 +0200

    url e reload

--- event/main_listener.php
+++ event/main_listener.php
@@ -21,6 +21,7 @@ class main_listener implements EventSubscriberInterface
 	$whoisonline = true; //refresh who is online
 	$forumlist = true;   //refresh forum list
 	$posts_intopics = true;   //refresh posts in view topics
+	$reload_seconds = 15;   //refresh time in seconds
 	
 	static public function getSubscribedEvents()
 	{
@@ -28,19 +29,22 @@ class main_listener implements EventSubscriberInterface
 			'core.common'		=> 'variables_refresh',
 			);
 	}
-	/* @var \phpbb\template\template */
 	protected $config;
+	protected $request;
 
-	public function __construct(\phpbb\config\config $config)
+	public function __construct(\phpbb\config\config $config, \phpbb\request\request $request)
 	{
-        $this->$config = $config;
+        $this->config = $config;
+        $this->request = $request;
 	}
 	public function variables_refresh($event)
 	{
+	$url=utf8_normalize_nfc($this->request->variable('request_uri', '', true));
 	//change true to false to disable the options
 	$this->template->assign_var('FORUMLIST_RELOAD', $forumlist); 
 	$this->template->assign_var('WHOISONLINE_RELOAD', $whoisonline);
 	$this->template->assign_var('POSTS_RELOAD', $posts_intopics);
-	$this->template->assign_var('URL_CORRENTE', $what);
+	$this->template->assign_var('URL_CORRENTE', $url);
+	$this->template->assign_var('TIME_RELOAD', $reload_seconds);
 	}
 }
