6dc6e2c8dfeb6df8b13a0e67ef82fadf225cecfb
bruninoit <brunino98@hotmail.it>
2015-05-04 11:37:39 +0200

    info

---
+++ acp/main_info.php
@@ -0,0 +1,23 @@
+<?php
+/** 
+* 
+* @package StaffIt - Ajax Notifications 
+* @copyright (c) 2014 brunino
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2 
+* 
+*/ 
+namespace bruninoit\ajaxreload\acp;
+class main_info
+{
+	function module()
+	{
+		return array(
+			'filename'	=> '\bruninoit\ajaxreload\acp\main_module',
+			'title'		=> 'ACP_AR_TITLE',
+			'version'	=> '1.0.0',
+			'modes'		=> array(
+				'settings'	=> array('title' => 'ACP_AR_TITLE', 'auth' => 'ext_brununoit/ajaxreload && acl_a_board', 'cat' => array('ACP_AR_TITLE')),
+			),
+		);
+	}
+}
