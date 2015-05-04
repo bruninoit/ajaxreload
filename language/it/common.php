7abd8b27cd74f3934c6b99896b9cfb34fa53b701
bruninoit <brunino98@hotmail.it>
2015-05-04 14:31:46 +0200

    solo modulo

--- language/it/common.php
+++ language/it/common.php
@@ -15,26 +15,7 @@ if (empty($lang) || !is_array($lang))
 	$lang = array();
 }
 $lang = array_merge($lang, array(
-	'LAST_TOPICS'			=> 'Ultimi Topic',
-    'LAST_POSTS'			=> 'Ultimi Post',
-	'TOP_TOPICS'			=> 'Argomenti più visti',
-	'ACP_TTT_TITLE'			=> 'Impostazioni generali',
-	'ACP_TTT_CATEGORY'		=> 'Top Ten Topics',
-	
-	'AFTER_FORUMLIST'		=> 'Dopo la lista dei forum e del box di login',
-	'AFTER_MARKFORUM'		=> 'Prima della lista dei forum, ma dopo link "segna come gi&agrave; letti"',
-	'BEFORE_LOGIN' 			=> 'Dopo la lista dei forum, ma prima del box di login',
-	'ALL_FOOTER'			=> 'In tutte le pagine, nel footer',
-	'ACP_TTT_SAVED'			=> 'Impostazioni della Top Ten Topics aggiornate',
-	'SIXMONTH' => 'ultimi 6 mesi',
-	'TWELVEMONTH' => 'ultimi 12 mesi',
-	'ONEMONTH' => 'ultimo mese',
-	'THREEMONTH' => 'ultimi 3 mesi',
-	'ALL_TOPICS' => 'Tutti',
-	'TTT_BASED' => 'basata sulla MOD di <a href="http://giannidose.altervista.org">Micogian</a>',
-	'TNORMAL' => 'Solo topic normali',
-	'TIMPORTANT' => 'Topic normali e importanti',
-	'TANNUNCE' => 'Topic normali, importanti e annunci',
-	'TGLOBAL' => 'Topic normali, importanti, annunci e annunci globali',
-	'TTT_BY' => 'di',
+	'ACP_AR_TITLE'			=> 'Impostazioni',
+	'ACP_AR_CATEGORY'		=> 'Real Time Notifications ',
+	'ACP_AR_SAVED'			=> 'Impostazioni aggiornate correttamente',
 ));
